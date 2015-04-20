6cc4bb16dcc5c38104c28263affac01e8155fc41
iRoxy <raquel_blackrose@hotmail.com>
2015-04-20 13:33:57 -0400

    Update Room.java

--- Afterschool/src/Room.java
+++ Afterschool/src/Room.java
@@ -1,171 +1,138 @@
-import java.awt.Component;
-import java.awt.List;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Scanner;
 
-/**
- * @author Elisha Patterson
- * This is the Room class which contains and manages all the rooms
- * and interaction with those rooms within the After School game.
- * 
- */
-
-public class Room{
-
-	Room roomName;
-	String roomDescription;
-	Item itemInRoom;
-	Monster monsterInRoom;
-	NPC npcInRoom;
-
-	/**
-	 * @param roomName
-	 * @param roomDescription
-	 * @param itemInRoom
-	 * @param monsterInRoom
-	 * @param npcInRoom
-	 * 
-	 * creating Room constructors
-	 */
-	public Room(Room roomName, String roomDescription,  Item itemInRoom, Monster monsterInRoom, NPC npcInRoom) {
-
-		this.roomName= roomName;
-		this.roomDescription= roomDescription;
-		this.itemInRoom= itemInRoom;
-		this.monsterInRoom= monsterInRoom;
-		this.npcInRoom= npcInRoom;
-	}
-
-
-	public Room() {}
-	
-	/**
-	 * method that contains a list of items
-	 * and returns the most current item.
-	 *
-	 */
-	
-	/**
-	 * method that contains a list of rooms
-	 * and returns the most current room.
-	 *
-	 */
-	public String currentRoom(String currentRoom)
-	{
-		return currentRoom;
-	}
-	
-	public String currentItem(String itemInRoom)
-	{
-		return itemInRoom;
-	}
-	
-	/**
-	 * method that contains a list of monsters
-	 * and returns the most current monster.
-	 *
-	 */
-	public String currentMonster(String monsterInRoom)
-	{
-		return monsterInRoom;
-	}
-	
-	//note: did not make a currentNPC for NPC as there is only one kind of NPC.
-
-	/**
-	 * generating getters and setters
-	 *
-	 */
-	public Room getRoomName() {
-		return roomName;
-	}
-
-
-	public void setRoomName(Room roomName) {
-		this.roomName = roomName;
-	}
-
-
-	public String getRoomDescription() {
-		return roomDescription;
-	}
-
-
-	public void setRoomDescription(String roomDescription) {
-		this.roomDescription = roomDescription;
-	}
-
-
-	public Item getItemInRoom() {
-		return itemInRoom;
-	}
-
-
-	public void setItemInRoom(Item itemInRoom) {
-		this.itemInRoom = itemInRoom;
-	}
-
-
-	public Monster getMonsterInRoom() {
-		return monsterInRoom;
-	}
-
-
-	public void setMonsterInRoom(Monster monsterInRoom) {
-		this.monsterInRoom = monsterInRoom;
-	}
-
-
-	public NPC getNpcInRoom() {
-		return npcInRoom;
-	}
-
-
-	public void setNpcInRoom(NPC npcInRoom) {
-		this.npcInRoom = npcInRoom;
-	}
-
-
-	public Room[] getRoomNames() {
-		return roomNames;
-	}
-
-
-	public void setRoomNames(Room[] roomNames) {
-		this.roomNames = roomNames;
-	}
-
-	Room[] roomNames = new Room[30];
-
-
-	/**stores and returns the room descriptions
-	 * 
-	 */
-	//	public String assignDescription()
-	//	{
-	//		
-	//	}
-
-	/**
-	 * @Override
-	 * The toString method to display the contents of the room to the user.
-	 *
-	 */
-	public String toString()
-	{
-		System.out.println("The items in this room are: " + itemInRoom + '\n');
-		System.out.println("The monsters in this room are: " + monsterInRoom + '\n');
-		System.out.println("The NPC in this room are: " + npcInRoom);
-
-		return null;
-	}
-
-
-public static void main(String[] args)
+public class Room implements Comparable<Room>
 {
-	Room room = new Room();
-	System.out.println(room.getNpcInRoom().toString());
+	private String name;
+	private String desc;
+	private Monster monster;
+	private NPC npc;
+	private Item item;
+	private enum REGION {MUDDYPOND,ZONE,YARD};
+	private ArrayList<Room> map;
 	
-}
+	public Room()
+	{
+		map = createRooms();
+	}
+	
+	public Room(String name, String desc, Monster monster, NPC npc, Item item, REGION region)
+	{
+		this.name = name;
+		this.desc = desc;
+		this.monster = monster;
+		this.npc = npc;
+		this.item = item;
+	}
+
+	public String getName() 
+	{
+		return name;
+	}
+
+	public void setName(String name) 
+	{
+		this.name = name;
+	}
+
+	public String getDesc() 
+	{
+		return desc;
+	}
+
+	public void setDesc(String desc) 
+	{
+		this.desc = desc;
+	}
+
+	public Monster getMonster() 
+	{
+		return monster;
+	}
+
+	public void setMonster(Monster monster) 
+	{
+		this.monster = monster;
+	}
+
+	public NPC getNpc() 
+	{
+		return npc;
+	}
+
+	public void setNpc(NPC npc) 
+	{
+		this.npc = npc;
+	}
+
+	public Item getItem() 
+	{
+		return item;
+	}
+
+	public void setItem(Item item) 
+	{
+		this.item = item;
+	}
+	
+	
+	public ArrayList<Room> createRooms()
+	{
+		ArrayList<Room> rooms = new ArrayList<Room>();
+		rooms.add(new Room("Room 1", "", null, null, null,REGION.ZONE));
+		rooms.add(new Room("Room 2", "", null, null, null,REGION.ZONE));
+		rooms.add(new Room("Room 3", "", null, null, null,REGION.ZONE));
+		rooms.add(new Room("Room 4", "", null, null, null,REGION.ZONE));
+		rooms.add(new Room("Room 5", "", null, null, null,REGION.MUDDYPOND));
+		rooms.add(new Room("Room 6", "", null, null, null,REGION.MUDDYPOND));
+		rooms.add(new Room("Room 7", "", null, null, null,REGION.MUDDYPOND));
+		rooms.add(new Room("Room 8", "", null, null, null,REGION.YARD));
+		rooms.add(new Room("Room 9", "", null, null, null,REGION.YARD));
+		rooms.add(new Room("Room 10", "", null, null, null,REGION.YARD));
+		
+		return rooms;
+	}
+	
+
+	public ArrayList<Room> getMap() 
+	{
+		return map;
+	}
+
+	public void setMap(ArrayList<Room> map) 
+	{
+		this.map = map;
+	}
+
+	@Override
+	public int compareTo(Room o) 
+	{
+		final int equal = 0;
+		final int notEqual = 1;
+		
+		if(this.getName().equals(o.getName()))
+		{
+			return equal;
+		}
+		
+		else
+		{
+			return notEqual; 
+		}
+	}
+	
+	public void nextRoom(ArrayList<Room> theMap, User player)
+	{
+		Room room = null;
+		Room currentRoom = player.getCurrentRoom();
+		for(int i = 0; i < theMap.size(); i++)
+		{
+			Room roomInList = theMap.get(i);
+			if(roomInList.compareTo(currentRoom) == 0)
+			{
+				room = theMap.get(i + 1);
+				player.setCurrentRoom(room);
+			}
+		}
+	}
+
 }
