955bf86ba1b639b53e991736cbe13b1c6d603403
iRoxy <raquel_blackrose@hotmail.com>
2015-04-22 12:02:18 -0400

    Update Game.java

--- Afterschool/src/Game.java
+++ Afterschool/src/Game.java
@@ -8,8 +8,8 @@ import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.PrintWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
-import javax.sound.sampled.Line;
 
 public class Game
 {
@@ -17,13 +17,21 @@ public class Game
 	private FileReader reader;
 	private BufferedReader bfread;
 	private File saveFile = new File("save.txt");
+	private User player;
+	private Room overallRoom;
+	private Puzzle puzzle;
 	
 	/**Method: Game constructor
 	 * This method constructs a game object
 	 * @throws IOException 
 	 */
-	public Game() 
+	public Game(User player, Room overallRoom, Puzzle puzzle) 
 	{
+		
+		this.player = player;
+		this.overallRoom = overallRoom;
+		this.puzzle = puzzle;
+		
 		try 
 		{
 			writer = new FileWriter(saveFile);
@@ -77,14 +85,74 @@ public class Game
 	{
 		this.saveFile = saveFile;
 	}
+	
+	/**Method: getUser
+	 * Retrieves the user object for the
+	 * game
+	 * @return the user object
+	 */
+	public User getPlayer() 
+	{
+		return player;
+	}
 
+	/**Method: setPlayer
+	 * This method sets the player object for
+	 * the Game class
+	 * @param player - the user object
+	 */
+	public void setPlayer(User player) 
+	{
+		this.player = player;
+	}
+	
+	/**Method: getOverallRoom
+	 * Retrieves the room object for the 
+	 * Game class
+	 * @return overallRoom object 
+	 */
+	public Room getOverallRoom() 
+	{
+		return overallRoom;
+	}
+
+	/**Method: setOverallRoom
+	 * This method sets the overall room
+	 * object for the Game class
+	 * @param overallRoom
+	 */
+	public void setOverallRoom(Room overallRoom) 
+	{
+		this.overallRoom = overallRoom;
+	}
+	
+
+	/**Method: getPuzzle
+	 * Retrieves the puzzle object for the
+	 * Game class
+	 * @return
+	 */
+	public Puzzle getPuzzle() 
+	{
+		return puzzle;
+	}
+
+	/**Method: setPuzzle
+	 * Method sets the puzzle object for the
+	 * Game class
+	 * @param puzzle
+	 */
+	public void setPuzzle(Puzzle puzzle) 
+	{
+		this.puzzle = puzzle;
+	}
 
 
 	/**Method: startGame
 	 * This method starts the game
 	 * @return the created User object
 	 */
-	public User startGame()
+	public static User startGame()
 	{
 		
 		System.out.println("Please enter your name: ");
@@ -143,36 +211,84 @@ public class Game
 		}
 	}
 	
-	//method that continuously gets called within a loop that 
-	//asks for user input 
+	/**Method: input
+	 * This method gets asks for the user input
+	 * and calls to a method that uses input
+	 * to drive the game
+	 */
 	public void input()
 	{
 		String input = FileIO.readIn();
-		
 		checkForDirection(input);
+		
 	}
 	
-	
-	//Without roadblocks, without checks. Where user can freely
-	//proceed through 
-	private void checkForDirection(String line)
+	/**Method: checkForDirection
+	 * This method is a private method used within another
+	 * method. Uses user input to perform a variety 
+	 * of actions that drive the game
+	 */
+	private void checkForDirection(String input)
 	{
-		if(line.equalsIgnoreCase("next"))
+		ArrayList<Room> map = getOverallRoom().getMap();
+		User player = getPlayer();
+		
+		if(input.equalsIgnoreCase("forward"))
 		{
-			System.out.println("Hey, you're going forward");
+			Room.nextRoom(map, player);
+			System.out.println("You are now in: " + player.getCurrentRoom().getName());
 		}
 		
-		if(line.equalsIgnoreCase("back"))
+		else if(input.equalsIgnoreCase("back"))
 		{
-			System.out.println("Hey, you're going back");
+			Room.previousRoom(map, player);
+			System.out.println("You are now in " + player.getCurrentRoom().getName() + " once again");
 		}
 		
-		if(line.equalsIgnoreCase("search"))
+		else if(input.equalsIgnoreCase("exit"))
 		{
-			System.out.println("Hey, you're searching");
+			exitGame();
 		}
 		
+		else if(input.equalsIgnoreCase("help"))
+		{
+			System.out.println("Hey, you went for help");
+		}
+		
+		else if (input.equalsIgnoreCase("save"))
+		{
+			saveGame(getPlayer());
+		}
+		
+		else
+		{
+			System.out.println("Please enter the commands either: forward, back, exit, help or save");
+		}
 	}
+	
+	/**Method: puzzleScenario
+	 * This method gives the user a riddle to solve
+	 * until correct
+	 */
+	public void puzzleScenario()
+	{
+		NPC npc = getPlayer().getCurrentRoom().getNpc();
+		Puzzle puzzle = getPuzzle();
+		npc.talk();
+		
+		if(npc.isHasRiddle())
+		{
+			Puzzle.giveRiddle(puzzle);
+		}
+		
+		else
+		{
+
+		}
+
+	}
+	
+
 
 }
 
