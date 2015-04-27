5bfb878d4b8028a07dbbb98411e6376fbe3ac997
iRoxy <raquel_blackrose@hotmail.com>
2015-04-26 23:44:40 -0400

    Create Help.java

---
+++ Afterschool/src/Help.java
@@ -0,0 +1,23 @@
+/**
+ * @author Raquel Lawrene
+ */
+
+public class Help 
+{
+	
+	/**Method: howToPlay
+	 * Method that prints out common gameplay instructions
+	 * to the user when asked for
+	 */
+	public static void howToPlay()
+	{
+		System.out.println("\nTo play this game: Please type in the following commands with correct spelling" + "\n"
+				+ "\n" + "forward : to move on to the next room"
+				+ "\n" + "back : to move to the previous room you were just in"
+				+ "\n" + "exit : to exit the game. (It is recommended that you save the game before typing this command"
+				+ "\n" + "search : to serac for items that will be useful in a room"
+				+ "\n" + "save : save your current progress in game in a previous load item"
+				+ "\n" + "help : type this command at anytime to bring up this current menu ");
+	}
+
+}
